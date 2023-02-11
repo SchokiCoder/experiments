8f2330ed1ea0b75e2d6ceba52822b3eb87ee0c81
SchokiCoder <afschoknecht@gmail.com>
2023-02-11 15:10:08 +0100

    add goals

---
+++ c/cgol.c
@@ -0,0 +1,9 @@
+/* Copyright (c) 2023, Andy Frank Schoknecht
+ * Use of this source code is governed by the BSD-3-Clause
+ * license, that can be found in the LICENSE file.
+ */
+
+int main()
+{
+	return 0;
+}
---
+++ docs/goals.md
@@ -0,0 +1,7 @@
+
+# Goals
+
+To implement a Conway's Game of Life in various languages.
+
+- 80 x 24
+
