dd0f7a0852c616bce68629be1a1fe3487fa7b7f9
SchokiCoder <afschoknecht@gmail.com>
2023-04-06 12:06:52 +0200

    add zig cgol (wip)

--- Makefile
+++ Makefile
@@ -1,26 +1,36 @@
-C_CC = cc
+CC = cc
 C_COPTS = -std=c99 -pedantic
 C_SRC = c/cgol.c
 
-GO_CC = go build
+ZIGC = zig build-exe
+ZIG_OPTS = 
+ZIG_SRC = zig/cgol.zig
+
+GOC = go build
 GO_OPTS =
 GO_SRC = go/cgol.go
 
-cgol: c_cgol go_cgol
+cgol: c go zig
 
-debug: c_debug go_debug
+debug: c_debug go_debug zig_debug
 
-c_cgol: clean
-	$(C_CC) ${C_COPTS} -Os -o bin/$@ ${C_SRC}
+c: clean
+	$(CC) ${C_COPTS} -Os -o bin/$@ ${C_SRC}
 
 c_debug: clean
-	$(C_CC) ${C_COPTS} -g -o bin/c_cgol ${C_SRC} -Wall -Wextra
+	$(CC) ${C_COPTS} -g -o bin/c ${C_SRC} -Wall -Wextra
 
-go_cgol:
-	$(GO_CC) ${GO_OPTS} -o bin/$@ ${GO_SRC}
+zig:
+	$(ZIGC) ${ZIG_OPTS} -o bin/$@ ${ZIG_SRC} -O3
+
+zig_debug:
+	$(ZIGC) ${ZIG_OPTS} -o bin/$@ ${ZIG_SRC}
+
+go:
+	$(GOC) ${GO_OPTS} -o bin/$@ ${GO_SRC}
 
 go_debug:
-	$(GO_CC) ${GO_OPTS} -o bin/go_cgol ${GO_SRC}
+	$(GOC) ${GO_OPTS} -o bin/go ${GO_SRC}
 
 clean:
 	rm -f bin/*
---
+++ zig/cgol.zig
@@ -0,0 +1,141 @@
+/* Copyright (c) 2023, Andy Frank Schoknecht
+ * Use of this source code is governed by the BSD-3-Clause
+ * license, that can be found in the LICENSE file.
+ */
+
+const std = @import("std");
+
+const WORLD_MAX_X = 80;
+const WORLD_MAX_Y = 24;
+
+const World = struct {
+	w, h: u32,
+	fields: bool[WORLD_MAX_X][WORLD_MAX_Y],
+
+	pub fn clone(self: const World) World {
+		long unsigned x, y;
+		
+		for (x = 0; x < out->w; x += 1) {
+			for (y = 0; y < out->h; y += 1) {
+				in->fields[x][y] = out->fields[x][y];
+			}
+		}
+	}
+
+	pub fn count_neighbors(wld: const World, x: u32, y: u32) {
+		ret: u32 = 0;
+		
+		if (x > 0)
+			if (self.fields[x - 1][y])
+				ret += 1;
+
+		if (y > 0)
+			if (self.fields[x][y - 1])
+				ret += 1;
+
+		if (x < (self.w - 1))
+			if (self.fields[x + 1][y])
+				ret += 1;
+
+		if (y < (self.h - 1))
+			if (self.fields[x][y + 1])
+				ret += 1;
+
+		if (x > 0 && y > 0)
+			if (self.fields[x - 1][y - 1])
+				ret += 1;
+
+		if (x < (self.w - 1) && y > 0)
+			if (self.fields[x + 1][y - 1])
+				ret += 1;
+
+		if (x > 0 && y < (self.h - 1))
+			if (self.fields[x - 1][y + 1])
+				ret += 1;
+
+		if (x < (self.w - 1) && y < (self.h - 1))
+			if (self.fields[x + 1][y + 1])
+				ret += 1;
+		
+		return ret;
+	}
+
+	void eval(self: World)
+	{
+		neighbors: u32;
+		x, y: u32;
+		new_wld = World {
+			.w = self.w,
+			.h = self.h,
+		};
+		
+		new_wld = self.clone();
+		
+		for (x = 0; x < self.w; x += 1) {
+			for (y = 0; y < self.h; y += 1) {
+				neighbors = self.count_neighbors(x, y);
+
+				if (neighbors == 3)
+					new_wld.fields[x][y] = true;
+				else if (neighbors != 2)
+					new_wld.fields[x][y] = false;
+			}
+		}
+		
+		self = new_wld.clone();
+	}
+};
+
+pub fn main() void {
+	active: bool = true;
+	wld = World {
+		.w = WORLD_MAX_X,
+		.h = WORLD_MAX_Y
+	};
+	is_x = false;
+	i, x, y: u32;
+	
+	for (x = 0; x < wld.w; x += 1) {
+		for (y = 0; y < wld.h; y += 1) {
+			wld.fields[x][y] = false;
+		}
+	}
+	
+	for (i = 1; i < argc; i += 1) {
+		if (is_x == 0) {
+			x = strtoul(argv[i], NULL, 10);
+			is_x = true;
+		} else {
+			y = strtoul(argv[i], NULL, 10);
+			wld.fields[x][y] = true;
+			is_x = false;
+		}
+	}
+	
+	while (active) {
+		for (y = 0; y < wld.h; y += 1) {
+			printf("\n");
+			
+			for (x = 0; x < wld.w; x += 1) {
+				if (wld.fields[x][y]) {
+					printf("x");
+				} else {
+					printf(" ");
+				}
+			}
+		}
+		
+		wld.eval();
+		
+		switch (fgetc(stdin)) {
+		case 'q':
+			active = false;
+			break;
+		
+		case EOF:
+			active = false;
+			fprintf(stderr, "Error end of input\n");
+			break;
+		}
+	}
+}
