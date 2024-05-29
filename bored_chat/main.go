425c32aec34219375336c11ad066f46bc1f3c053
SchokiCoder <afschoknecht@tutanota.com>
2024-05-29 23:38:47 +0200

    add to bored chat
    Toss TCP out, because the P2P clients would BOTH listen and then endlessly wait for a connection.
    Use UDP instead.

--- bored_chat/main.go
+++ bored_chat/main.go
@@ -11,10 +11,14 @@ import (
 	"strings"
 )
 
-const ReadMsgsDefaultNum = 10
+const (
+	ReadMsgResponse = "BC"
+	ReadMsgsDefaultNum = 10
+	SendMsgMaxTries = 10
+)
 
 func actionReadMessages(conn net.Conn, reader *bufio.Reader) {
-	fmt.Printf("Print X amount of recent messages: [%v] ",
+	fmt.Printf("Print X amount of recent messages: [%v]\n",
 		ReadMsgsDefaultNum)
 	//numMsgs := getInput(reader)
 	// TODO: conn.Read(numMsgs)
@@ -22,8 +26,27 @@ func actionReadMessages(conn net.Conn, reader *bufio.Reader) {
 
 func actionSendMessage(conn net.Conn, reader *bufio.Reader) {
 	fmt.Printf("Type message, and send with enter:\n")
-	//msg := getInput(reader)
-	// TODO: conn.Send(msg)
+	msg := getInput(reader)
+
+	for i := 0; i <= SendMsgMaxTries; i++ {
+		var err error
+		var response [10]byte
+
+		_, err = conn.Write([]byte(msg))
+		if err != nil {
+			continue
+		}
+
+		_, err = conn.Read(response[:])
+		if err != nil {
+			continue
+		}
+		if string(response[:]) == ReadMsgResponse {
+			break
+		} else {
+			continue
+		}
+	}
 }
 
 func getInput(reader *bufio.Reader) string {
@@ -34,31 +57,60 @@ func getInput(reader *bufio.Reader) string {
 	return s
 }
 
+func listen(conn net.Conn) {
+	var err error
+	var msg []byte
+	var msgsFile os.File
+
+	msgsFile, err = os.CreateTemp("", "bc_messages")
+	if err != nil {
+		panic("Could not create a temp file for messages.")
+	}
+	defer os.Remove(tempFile.Name())
+
+	for {
+		_, err = conn.Read(msg)
+		if err != nil {
+			panic("Should this even panic?")
+		}
+
+		if msg {
+			save to temp file
+
+			conn.Send(ReadMsgResponse)
+		}
+	}
+}
+
 func main() {
+	var action string
 	var reader = bufio.NewReader(os.Stdout)
 
-	conn, err := net.Dial("tcp", "127.0.0.1:82")
+	conn, err := net.Dial("udp", "127.0.0.1:81")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
+
+	go listen()
 
 	mainloop:
 	for {
 		fmt.Printf(`Actions:
 r:	Read messages
 s:	Send a message
-q:	Quit`)
-		action := getInput(reader)
+q:	Quit
+`)
+		action = getInput(reader)
 		action = strings.ToLower(action)
-		switch action {
-		case "r":
+		switch action[0] {
+		case 'r':
 			actionReadMessages(conn, reader)
 
-		case "s":
+		case 's':
 			actionSendMessage(conn, reader)
 
-		case "q":
+		case 'q':
 			break mainloop
 
 		default:
